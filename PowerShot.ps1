122ac53815c816518b4495ce4a6c89b6c3ee2df4
chono91 <chono91@gmail.com>
2015-11-04 23:12:16 -0500

    Update PowerShot.ps1

--- PowerShot.ps1
+++ PowerShot.ps1
@@ -1,15 +1,30 @@
-<# Registry access fix https://support.microsoft.com/en-us/kb/329291
-    sysinternals for psexec https://technet.microsoft.com/en-us/sysinternals/bb842062#>
+<#  
+sysinternals for psexec https://technet.microsoft.com/en-us/sysinternals/bb842062   (psexec -i -s powershell) 
+System doesn't have access to all regkeys, This may be OK and we can jsut hide the errors
+Files in use cannot be hashed, this may be OK and we can hide errors
+The Write-Progress indicators need to be tweaked
+#> 
 
 Function Get-PowerShot
 
 {
 $registryFolders = (Get-ChildItem -Path Registry::*).Name
 #foreach($regFolder in $registryFolders) {Get-RecentFiles -numberDays 1 -touchedFiles ([ref]$touchedFiles)  -path ("Registry::" + $regFolder)}
-
+$i = 1
 foreach ($regFolder in $registryFolders) {
-$regDir = "Registry::"+$regFolder 
-Get-ChildItem -Path $regDir -Recurse | Out-File -Append registryshot.reg}
+    Write-Progress -Activity "Registry Snapshot" -Status "Percent Complete" -PercentComplete ($i / $registryFolders.count * 100)
+    $regDir = "Registry::"+$regFolder 
+    Get-ChildItem -Path $regDir -Recurse | Out-File -Append registryshot2.reg
+}
 
-Get-ChildItem -Recurse | Get-FileHash -Algorithm MD5 | Out-File -Append filehashes.txt
+
+$i = 0
+
+$rootFolders = Get-ChildItem C:\
+foreach ($folder in $rootFolders){
+    Write-Progress -Activity "Filesystem Snapshot" -Status "Percent Complete" -PercentComplete ($i /$rootFolders.count * 100)
+    Get-ChildItem C:\$folder -Recurse | Get-FileHash -Algorithm MD5 | Out-File -Append filehashes2.txt
+    $i++
+}
+
 }
