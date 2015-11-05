ab2bf3006785aedb9d1232a7e7db7b0e27534371
chono91 <chono91@gmail.com>
2015-11-04 21:23:59 -0500

    Create PowerShot.ps1

---
+++ PowerShot.ps1
@@ -0,0 +1,15 @@
+<# Registry access fix https://support.microsoft.com/en-us/kb/329291
+    sysinternals for psexec https://technet.microsoft.com/en-us/sysinternals/bb842062#>
+
+Function Get-PowerShot
+
+{
+$registryFolders = (Get-ChildItem -Path Registry::*).Name
+#foreach($regFolder in $registryFolders) {Get-RecentFiles -numberDays 1 -touchedFiles ([ref]$touchedFiles)  -path ("Registry::" + $regFolder)}
+
+foreach ($regFolder in $registryFolders) {
+$regDir = "Registry::"+$regFolder 
+Get-ChildItem -Path $regDir -Recurse | Out-File registryshot.reg}
+
+Get-ChildItem -Recurse | Get-FileHash -Algorithm MD5 | Out-File -Append filehashes.txt
+}
